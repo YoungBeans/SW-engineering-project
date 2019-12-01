9ed5d7db4fdc7da08ef3be90a02df87c087294d2
YoungBeans <klog123480@gmail.com>
2019-12-02 01:21:05 +0900

    test4

--- reservation/reserv/tests.py
+++ reservation/reserv/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
-from django.urls import resolve
-from .views import *
-# Create your tests here.
+# from django.urls import resolve
+# from django.http import HttpRequest
+# from .views import home
+# # Create your tests here.
 
-class HomePageTest(TestCase) :
-    def test_root_url_resolves_to_home_page_view(self) :
-        found = resolve('/')
-        self.assertEqual(found.func, home)
+# class HomePageTest(TestCase) :
+#     def test_root_url_resolves_to_home_page_view(self) :
+#         found = resolve('/')
+#         self.assertEqual(found.func, home)
+
+#     def test_home_page_returns_correct_html(self) :
+#         request = HttpRequest()
+#         response = home(request)
+#         html = response.content.decode('utf8')
+#         self.assertTrue(html.startswith('<html>'))
+#         self.assertIn('<title>Galpung</title>', html)
+#         self.assertTrue(html.endswith('</html>'))
