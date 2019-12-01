2d26d4e64ea6ea7325ee28ef610341f4f693dca1
YoungBeans <klog123480@gmail.com>
2019-12-02 01:35:01 +0900

    test5

--- reservation/reserv/tests.py
+++ reservation/reserv/tests.py
@@ -1,18 +1,36 @@
 from django.test import TestCase
-# from django.urls import resolve
-# from django.http import HttpRequest
-# from .views import home
-# # Create your tests here.
+from django.urls import resolve
+from django.http import HttpRequest
+from .views import reserv_home, reserv_check, reserv_modify_render
+# Create your tests here.
 
-# class HomePageTest(TestCase) :
+class ReservHome(TestCase) :
+    def test_root_url_resolves_to_home_page_view(self) :
+        found = resolve('/reserv/')
+        self.assertEqual(found.func, reserv_home)
+
+    def test_home_page_returns_correct_html(self) :
+        response = self.client.get('/reserv/')
+        self.assertTemplateUsed(response, 'reserv/reservate.html')
+
+    def test_can_save_a_POST_request(self) :
+        response = self.client.post('/reserv/', data={'date' : '2020-05-15T13:12', 'hopping_member' : '3'})
+        self.assertIn('date', response.content.decode())
+
+class ReservCheck(TestCase) :
+    def test_root_url_resolves_to_home_page_view(self) :
+        found = resolve('/reserv/check/')
+        self.assertEqual(found.func, reserv_check)
+
+    def test_home_page_returns_correct_html(self) :
+        response = self.client.get('/reserv/check/')
+        self.assertTemplateUsed(response, 'reserv/reservation_check.html')
+
+# class Reserv_modify_render(TestCase) :
 #     def test_root_url_resolves_to_home_page_view(self) :
-#         found = resolve('/')
-#         self.assertEqual(found.func, home)
+#         found = resolve('/reserv/check/')
+#         self.assertEqual(found.func, reserv_check)
 
 #     def test_home_page_returns_correct_html(self) :
-#         request = HttpRequest()
-#         response = home(request)
-#         html = response.content.decode('utf8')
-#         self.assertTrue(html.startswith('<html>'))
-#         self.assertIn('<title>Galpung</title>', html)
-#         self.assertTrue(html.endswith('</html>'))
+#         response = self.client.get('/reserv/check/')
+#         self.assertTemplateUsed(response, 'reserv/reservation_check.html')
