0915605d2360ce0011c1967b4310f3af178be1c2
YoungBeans <klog123480@gmail.com>
2019-12-02 01:05:30 +0900

    test1

--- reservation/function_tests.py
+++ reservation/function_tests.py
@@ -1,9 +1,22 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+options = Options()
+options.binary_location = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+class NewVisitorTest(unittest.TestCase) :
+    def setUp(self) :
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path=r"C:\Users\alkad\Desktop\chromedriver_win32\chromedriver.exe", )
 
-assert 'Django' in browser.title
+    def tearDown(self) :
+        self.driver.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self) :
+        self.driver.get("http://localhost:8000")
+
+        self.assertIn('Galpung', self.driver.title)
+        self.fail('테스트 종료')
+
+if __name__ == '__main__' :
+    unittest.main(warnings='ignore')
